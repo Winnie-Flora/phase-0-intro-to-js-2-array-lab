666bb4313e4772054eaac157ebd7f9b713eadbb5
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-07-01 22:37:42 +0300

    Adds array lab

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    return [...cats, "Broom"];
+}
+
+function prependCat(name){
+    return ["Arnold",  ...cats];
+}
+
+function removeLastCat(){
+    return [...cats.slice(0,2)];
+}
+
+function removeFirstCat(){
+    return [...cats.slice(1,3)];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
